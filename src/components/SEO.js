9e0efeb0810bae89b3989e9f583d4ed34a6569a6
ryanchou-dev <ryanchou111@gmail.com>
2024-06-01 15:20:22 -0700

    quickfix

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,15 +1,13 @@
 import Head from "next/head";
-import fixed from "../../data/fixed";
 
-
-const fixed = {
+const fixedData = {
 	title: "Ryan Chou | Student from California Designing for the Web. ",
 	desc: "Project Manager @ Competitive Programming Initiative, promoting competitive programming through resources such as the USACO Guide. I'm fascinated with web development, artificial intelligence, competitive programming, and design. In my free time, I enjoy chugging fruit tea, graphic design, and vibing to music B). If you want to learn more about me, check out my GitHub to see other cool projects that I've worked on."
 }
 export default function SEO({ title, desc }) {
 	return (
 		<Head>
-			<title>{title ? title : fixed.title}</title>
+			<title>{title ? title : fixedData.title}</title>
 			<link
 				rel="apple-touch-icon"
 				sizes="180x180"
@@ -29,14 +27,14 @@ export default function SEO({ title, desc }) {
 			/>
 			<link rel="manifest" href="/site.webmanifest" />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			<meta name="title" content={title ? title : fixed.title} />
-			<meta name="description" content={desc ? desc : fixed.desc} />
+			<meta name="title" content={title ? title : fixedData.title} />
+			<meta name="description" content={desc ? desc : fixedData.desc} />
 			<meta property="og:type" content="website" />
 			<meta property="og:url" content="https://www.ryanchou.dev/" />
-			<meta property="og:title" content={title ? title : fixed.title} />
+			<meta property="og:title" content={title ? title : fixedData.title} />
 			<meta
 				property="og:description"
-				content={desc ? desc : fixed.desc}
+				content={desc ? desc : fixedData.desc}
 			/>
 			<meta
 				property="og:image"
@@ -46,11 +44,11 @@ export default function SEO({ title, desc }) {
 			<meta property="twitter:url" content="https://www.ryanchou.dev" />
 			<meta
 				property="twitter:title"
-				content={title ? title : fixed.title}
+				content={title ? title : fixedData.title}
 			/>
 			<meta
 				property="twitter:description"
-				content={desc ? desc : fixed.desc}
+				content={desc ? desc : fixedData.desc}
 			/>
 			<meta
 				property="twitter:image"
