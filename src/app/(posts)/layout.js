d8fd34b09e000dda34b403e023963bf053c76c5c
ryanchou-dev <ryanchou111@gmail.com>
2024-06-07 21:43:39 -0700

    remove node error

--- src/app/(posts)/layout.js
+++ src/app/(posts)/layout.js
@@ -21,11 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<section>
-			<div className={inter.className + " bg-white  font-sans flex items-center  justify-center"}>
-				<Navbar />
-
-				<div className="w-full max-w-lg lg:max-w-3xl xl:max-w-5xl flex flex-col justify-center lg:flex-row gap-8 px-6 pb-8 z-[1]">{children}</div>
-			</div>
+			<div className="w-full max-w-lg lg:max-w-3xl xl:max-w-5xl flex flex-col justify-center lg:flex-row gap-8 px-6 pb-8 z-[1]">{children}</div>
 		</section>
 	);
 }
--- src/app/(projects)/projects/page.js
+++ src/app/(projects)/projects/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { projects } from "../../../public/projects";
+import { projects } from "../../../../public/projects"
 
 export default function Projects() {
 	return (
